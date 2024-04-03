ebf546bc277952c7ce48a669f885131818340afc
Gustavo Juliano Borges <awkzdev@gmail.com>
2024-04-03 00:19:13 -0300

    get recent achievements and completion progress

--- gamesAchievements.js
+++ gamesAchievements.js
@@ -6,13 +6,37 @@ const webApiKey = process.env.APIKEY;
 
 const authorization = buildAuthorization({ userName, webApiKey });
 
-const { getGame, getUserProfile } = require("@retroachievements/api");
+const { getAchievementsEarnedBetween, getUserCompletionProgress } = require("@retroachievements/api");
 
-async function main() {
-    const userProfile = await getUserProfile(authorization, {
+async function getRecentAchievements() {
+
+    const toDate = new Date();
+    const fromDate = new Date(toDate);
+    fromDate.setDate(toDate.getDate() - 5);
+
+    const achievements = await getAchievementsEarnedBetween(authorization, {
+        userName: "Awkz19",
+        fromDate: fromDate,
+        toDate: toDate,
+    });
+    console.log(achievements)
+
+}
+
+
+async function getCompletionProgress(){
+
+
+    const userCompletionProgress = await getUserCompletionProgress(authorization, {
         userName: "Awkz19",
       });
-    console.log(userProfile)
+    console.log(userCompletionProgress)
+
+}
+
+async function main() {
+    await getRecentAchievements()
+    await getCompletionProgress();
 }
 
 main();
