65ecb262838315668e9fea71c67b138dfb1ad9c5
Gustavo Juliano Borges <awkzdev@gmail.com>
2024-04-08 03:22:21 -0300

    Verify if game exists, set interval, currentDate to log

--- gamesAchievements.js
+++ gamesAchievements.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
-const fs = require('fs').promises
+const fs = require('fs').promises;
 const { buildAuthorization } = require("@retroachievements/api");
+
+require('dotenv').config();
 
 const userName = "Awkz19";
 const webApiKey = process.env.APIKEY;
 
 const authorization = buildAuthorization({ userName, webApiKey });
 
-const { getConsoleIds, getUserCompletionProgress } = require("@retroachievements/api");
+const { getUserCompletionProgress } = require("@retroachievements/api");
 
 async function getCompletionProgress() {
     const userCompletionProgress = await getUserCompletionProgress(authorization, {
@@ -17,43 +18,68 @@ async function getCompletionProgress() {
 }
 
 function getConsoleImage(consoleName) {
-    if (consoleName === "PlayStation 2") {
-        return "https://static.retroachievements.org/assets/images/system/ps2.png";
-    } else if (consoleName === "Game Boy") {
-        return "https://static.retroachievements.org/assets/images/system/gb.png";
-    } else if (consoleName === "Game Boy Advanced") {
-        return "https://static.retroachievements.org/assets/images/system/gba.png";
-    } else if (consoleName === "SNES/Super Famicom") {
-        return "https://static.retroachievements.org/assets/images/system/snes.png";
-    } else {
-        return "Console name error"; 
+    switch (consoleName) {
+        case "PlayStation 2":
+            return "https://static.retroachievements.org/assets/images/system/ps2.png";
+        case "Game Boy":
+            return "https://static.retroachievements.org/assets/images/system/gb.png";
+        case "Game Boy Advanced":
+            return "https://static.retroachievements.org/assets/images/system/gba.png";
+        case "SNES/Super Famicom":
+            return "https://static.retroachievements.org/assets/images/system/snes.png";
+        default:
+            return "Console name error";
     }
 }
 
 async function main() {
+    try {
 
-    //const consoleIds = await getConsoleIds(authorization);
-    //console.log(consoleIds)
+        const userCompletionProgress = await getCompletionProgress();
+        const results = userCompletionProgress.results;
 
-    const userCompletionProgress = await getCompletionProgress();
-    const results = userCompletionProgress.results;
 
-    const formattedData = results.map(game => ({
-        gameID: game.gameId,
-        name: game.title,
-        image: "https://media.retroachievements.org/" + game.imageIcon,
-        totalAchievements: game.maxPossible,
-        unlocked: game.numAwarded,
-        progression: Math.round((game.numAwarded / game.maxPossible) * 100), 
-        console: game.consoleName,
-        consoleImage: getConsoleImage(game.consoleName) 
-    }));
+        let existingData = [];
+        try {
+            const fileContent = await fs.readFile('database.json', 'utf8');
+            existingData = JSON.parse(fileContent);
+        } catch (error) {
+            console.log("O arquivo database.json não existe ou está vazio.");
+        }
 
-    const jsonData = JSON.stringify(formattedData, null, 2);
-    await fs.writeFile('database.json', jsonData);
-    console.log('JSON salvo com sucesso!');
+        results.forEach(newGame => {
+            const existingGameIndex = existingData.findIndex(existingGame => existingGame.gameID === newGame.gameId);
+            if (existingGameIndex !== -1) {
+                existingData[existingGameIndex] = {
+                    ...existingData[existingGameIndex],
+                    totalAchievements: newGame.maxPossible,
+                    unlocked: newGame.numAwarded,
+                    progression: Math.round((newGame.numAwarded / newGame.maxPossible) * 100),
+                    console: newGame.consoleName,
+                    consoleImage: getConsoleImage(newGame.consoleName)
+                };
+            } else {
+                existingData.push({
+                    gameID: newGame.gameId,
+                    name: newGame.title,
+                    image: "https://media.retroachievements.org/" + newGame.imageIcon,
+                    totalAchievements: newGame.maxPossible,
+                    unlocked: newGame.numAwarded,
+                    progression: Math.round((newGame.numAwarded / newGame.maxPossible) * 100),
+                    console: newGame.consoleName,
+                    consoleImage: getConsoleImage(newGame.consoleName)
+                });
+            }
+        });
 
+        await fs.writeFile('database.json', JSON.stringify(existingData, null, 2));
+        const currentDate = new Date().toLocaleString();
+        console.log(`[${currentDate}] Arquivo .json atualizado`);
+
+    } catch (error) {
+        console.error("Ocorreu um erro:", error);
+    }
 }
 
-
-main();
+main()
+setInterval(main, 12 * 3600000);
